07eac18c9a0fc5097211d9ca62ac988437d8de9b
Prisa Damrongsiri <prisa1987@gmail.com>
2019-10-18 11:28:13 +0100

    Initial Commit

---
+++ SchoolsMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SchoolsMap
+//
+//  Created by prisa dumrongsiri on 18/10/2562 BE.
+//  Copyright © 2562 pd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
